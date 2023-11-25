607e166ee287107655831fbe1fe52ca77839749f
Nathan Aguiar Barbosa Silva <nathanaguiarbs@gmail.com>
2023-11-25 15:11:16 -0300

    utilizando o Padrão ISO 8601 UTC para datas

--- src/main/java/com/ead/authuser/model/UserModel.java
+++ src/main/java/com/ead/authuser/model/UserModel.java
@@ -51,11 +51,11 @@ public class UserModel implements Serializable {
     private String imageUrl;
 
     @Column(nullable = false)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     private LocalDateTime creationDate;
 
     @Column(nullable = false)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     private LocalDateTime lastUpdateDate;
 
 }
